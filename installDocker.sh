11fb95c3e2629ba07b60cf1aa66ca59da5daa011
Jazys <jacquemet.julien@gmail.com>
2021-08-21 22:24:29 +0000

    add htpasswd for making hashpassword

--- installDocker.sh
+++ installDocker.sh
@@ -2,6 +2,7 @@
 
 [ `whoami` = root ] || exec sudo su -c $0
 apt update
+sudo apt-get install apache2-utils -y
 apt install apt-transport-https ca-certificates curl software-properties-common -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 add-apt-repository 'deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable'
