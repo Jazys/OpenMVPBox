45143ace0d1d1d2c05c9cfb30f8158b900614ce3
Jazys <jacquemet.julien@gmail.com>
2021-08-21 14:00:00 +0000

    add docker and docker-compose install

---
+++ installDocker.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+[ `whoami` = root ] || exec sudo su -c $0
+apt update
+apt install apt-transport-https ca-certificates curl software-properties-common -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+add-apt-repository 'deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable'
+apt update
+apt install docker-ce -y
+sudo curl -L "https://github.com/docker/compose/releases/download/1.26.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
