ad5618f6714b244664d24c47818d87142607a8fd
Jazys <jacquemet.julien@gmail.com>
2021-08-21 23:24:06 +0000

    install Traefik and portainer is Ok with this script

--- installManager.sh
+++ installManager.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+chmod +x traefik/install.sh
+./traefik/install.sh
+
 read -p "Indicate your domain for monitor traefik: " domaineTraefik
 sed -i 's/xxx.xxx/'$domaineTraefik'/g' traefik/conf/traefik_dynamic.toml
 
@@ -15,3 +18,12 @@ echo $EncryptedPassword
 
 #./replaceInFile.sh traefik/conf/traefik_dynamic.toml login:passcrypt $EncryptedPassword 
 sed -i 's/login:passcrypt/'$EncryptedPassword'/g' traefik/conf/traefik_dynamic.toml
+
+rm ./password
+
+read -p "Indicate your domain for portainer: " domainePortainer
+sed -i '/URL_PORTAINER/c\URL_PORTAINER = '$domainePortainer'' portainer/.env
+mkdir -p portainer/data
+
+docker-compose -f traefik/docker-compose.yml --env-file traefik/.env up -d
+docker-compose -f portainer/docker-compose.yml --env-file portainer/.env up -d
