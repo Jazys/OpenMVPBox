6bdffa1e2db17329df02d38c9915edc2126f5f7e
Jazys <jacquemet.julien@gmail.com>
2021-08-22 22:05:07 +0000

    remove https on env file during generation

--- appsmith/install-with-traefik.sh
+++ appsmith/install-with-traefik.sh
@@ -3,6 +3,6 @@
 mkdir -p ./data/mongo/db
 
 read -p "Indicate your domain for Appsmith: " domaineAppsmith
-sed -i '/URL_APPSMITH/c\URL_APPSMITH=https://'$domaineAppsmith'/' docker.env
+sed -i '/URL_APPSMITH/c\URL_APPSMITH='$domaineAppsmith'' docker.env
 
 docker-compose -f docker-compose-with-traefik.yml --env-file docker.env up -d
