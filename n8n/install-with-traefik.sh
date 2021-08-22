02efaf9fe5969f34e9004e54d568f11684729679
Jazys <jacquemet.julien@gmail.com>
2021-08-22 15:00:22 +0000

    add lauching docker on install

--- n8n/install-with-traefik.sh
+++ n8n/install-with-traefik.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
 mkdir -p ponstgres-data
-chown postgres:postgres postgres-data/ -R
+#chown postgres:postgres postgres-data/ -R
+#find better solution !!!
+chmod 777 postgres-data/ -R
+
 mkdir -p .n8n
 chmod +x init-data.sh
 
 read -p "Indicate your domain for n8n: " domaineN8n
 sed -i '/N8N_WEBHOOK_TUNNEL_URL/c\N8N_WEBHOOK_TUNNEL_URL=https://'$domaineN8n'/' .env
+
+docker-compose -f docker-compose-with-traefik.yml --env-file .env up -d
--- n8n/install.sh
+++ n8n/install.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
 mkdir -p ponstgres-data
-chown postgres:postgres postgres-data/ -R
+#chown postgres:postgres postgres-data/ -R
+#find better solution !!!
+chmod 777 postgres-data/ -R
 mkdir -p .n8n
 chmod +x init-data.sh
 
 sed -i '/N8N_WEBHOOK_TUNNEL_URL/c\N8N_WEBHOOK_TUNNEL_URL=http://'$(hostname  -I | cut -f1 -d' ')':5678/' .env
+
+docker-compose docker-compose.yml up -d
