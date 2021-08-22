225bdfd573d9085f19fe4d94e097bbd4cf0b973e
Jazys <jacquemet.julien@gmail.com>
2021-08-22 15:18:53 +0000

    add grav for making site

---
+++ appsmith/install-with-traefik.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+mkdir -p ./data/mongo/db
+
+read -p "Indicate your domain for Appsmith: " domaineAppsmith
+sed -i '/URL_APPSMITH/c\URL_APPSMITH=https://'$domaineAppsmith'/' docker.env
+
+docker-compose -f docker-compose-with-traefik.yml --env-file docker.env up -d
--- appsmith/install.sh
+++ appsmith/install.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 mkdir -p ./data/mongo/db
+docker-compose  --env-file docker.env up -d
