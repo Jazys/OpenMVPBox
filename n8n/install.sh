ca436963f60c25f48156455c9beba7470b5280d4
Jazys <jacquemet.julien@gmail.com>
2021-08-24 22:07:31 +0000

    fix error on lanching docker

--- n8n/install.sh
+++ n8n/install.sh
@@ -9,4 +9,4 @@ chmod +x init-data.sh
 
 sed -i '/N8N_WEBHOOK_TUNNEL_URL/c\N8N_WEBHOOK_TUNNEL_URL=http://'$(hostname  -I | cut -f1 -d' ')':5678/' .env
 
-docker-compose docker-compose.yml up -d
+docker-compose up -d
