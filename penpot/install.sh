8bb3ebe6051b356470705de83e12360676fc5ae1
Jazys <jacquemet.julien@gmail.com>
2021-08-27 22:22:19 +0000

    add penpot for using traefik again

--- penpot/install.sh
+++ penpot/install.sh
@@ -2,13 +2,14 @@
 
 [ `whoami` = root ] || exec sudo su -c $0
 
-wget https://raw.githubusercontent.com/penpot/penpot/main/docker/images/docker-compose.yaml
-wget https://raw.githubusercontent.com/penpot/penpot/main/docker/images/config.env
+#wget https://raw.githubusercontent.com/penpot/penpot/main/docker/images/docker-compose.yaml
+#wget https://raw.githubusercontent.com/penpot/penpot/main/docker/images/config.env
 
 #rm docker-compose.yaml
 #rm config.env
 
-sed -i '1,2d' config.env
+#sed -i '1,2d' config.env
 
-echo  "PENPOT_PUBLIC_URI=http://"$(hostname  -I | cut -f1 -d' ')":9001" >> config.env
+#echo  "PENPOT_PUBLIC_URI=http://"$(hostname  -I | cut -f1 -d' ')":9001" >> config.env
 
+sed -i 's/PENPOT_PUBLIC_URI/PENPOT_PUBLIC_URI=http://'$(hostname  -I | cut -f1 -d' ')':9001/g' config.env
