5528e16d00ecc51e3bf4358f69b2ee6beb883b84
Jazys <jacquemet.julien@gmail.com>
2021-08-29 13:40:28 +0000

    update installation script

--- automatTest/install-with-traefik.sh
+++ automatTest/install-with-traefik.sh
@@ -4,15 +4,15 @@
 
 mkdir -p dataClientSftp
 mkdir -p configSftp
+cp configSftp.json configSftp/sftp.json
 mkdir -p TestReport
+
 read -p "Indicate your domain for e2e test: " domaine
+sed -i 's/URL_REPORT=/URL_REPORT='$domaine'/g' .env 
 
-#sed -i 's/PENPOT_PUBLIC_URI/PENPOT_PUBLIC_URI='$domaine'/g' config.env
-#sed -i 's/URL_PENPOT/URL_PENPOT='$domainepenpot'/g' config.env 
+read -p "Indicate your domain for sftp client : " domaine
+sed -i 's/URL_CLI_SFTP=/URL_CLI_SFTP='$domaine'/g' .env 
 
-#docker-compose -f docker-compose-with-traefik.yml --env-file config.env up -data
+docker-compose -f docker-compose-with-traefik.yml up -d
 
-#echo "wait 30 second for starting docker"
-#sleep 30
 
-#docker exec -ti penpot_penpot-backend_1 ./manage.sh create-profile
