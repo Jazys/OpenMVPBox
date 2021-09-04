5c54ecdde4a8402e5b5d3c5462f5ee1fa100f137
Jazys <jacquemet.julien@gmail.com>
2021-09-04 13:47:22 +0000

    fix error and add user automatically for penpot

--- penpot/install-with-traefik.sh
+++ penpot/install-with-traefik.sh
@@ -6,12 +6,13 @@ mkdir postgres-data
 mkdir data
 
 read -p "Indicate your domain for penpot: " domainepenpot
-sed -i 's/PENPOT_PUBLIC_URI/PENPOT_PUBLIC_URI='$domainepenpot'/g' config.env
-sed -i 's/URL_PENPOT/URL_PENPOT='$domainepenpot'/g' config.env 
+sed -i '/PENPOT_PUBLIC_URI/c\PENPOT_PUBLIC_URI='$domainepenpot config.env
+sed -i '/URL_PENPOT/c\URL_PENPOT='$domainepenpot config.env 
 
-docker-compose -f docker-compose-with-traefik.yml --env-file config.env up -data
+docker-compose -f docker-compose-with-traefik.yml --env-file config.env up -d
 
 echo "wait 30 second for starting docker"
 sleep 30
 
-docker exec -ti penpot_penpot-backend_1 ./manage.sh create-profile
+docker exec -ti penpot_penpot-backend_1 ./manage.sh create-profile -u test@test.fr -p Azerty@1234 -n test
+
