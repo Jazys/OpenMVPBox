bc1a87c1d50212816db501f0c26d16b46e97c39c
Jazys <jacquemet.julien@gmail.com>
2021-09-04 10:46:04 +0000

    fix error on persistant data on postgres and fix error on setting  url in env file

--- strapi/install-with-traefik.sh
+++ strapi/install-with-traefik.sh
@@ -3,4 +3,7 @@
 mkdir -p postgres-data
 mkdir -p app
 
+read -p "Indicate your domain for strapi: " domaine
+sed -i '/URL_STRAPI/c\URL_STRAPI='$domaine .env
+
 docker-compose -f docker-compose-with-traefik.yml up -d 
