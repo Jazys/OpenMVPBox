b678f3d9abde046149c5bb9e19d24597d3dbbc0f
Jazys <jacquemet.julien@gmail.com>
2021-09-04 08:48:46 +0000

    codimd works with traefik, add some configuration for setting admin password

--- codimd/install-with-traefik.sh
+++ codimd/install-with-traefik.sh
@@ -3,6 +3,12 @@
 [ `whoami` = root ] || exec sudo su -c $0
 
 mkdir postgres-data
-mkdir data
+mkdir upload
 read -p "Indicate your domain for codi dashboard: " domaine
-sed -i 's/URL_CODI/URL_CODI='$domaine'/g' .env
+sed -i '/URL_CODI/c\URL_CODI='$domaine .env
+
+docker-compose -f docker-compose-with-traefik.yml up -d
+
+sleep 60
+
+docker exec -it codimd_codimd_1 /bin/bash -c 'cd bin && NODE_ENV=production ./manage_users --pass azerty --add admin@admin.fr'
