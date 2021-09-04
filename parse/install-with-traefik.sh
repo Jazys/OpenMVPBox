c7286e2b6c5fa679fc2b5e853a3dc66b18d3b529
Jazys <jacquemet.julien@gmail.com>
2021-09-04 14:34:01 +0000

    can change url in config.js, need also change other parameter

--- parse/install-with-traefik.sh
+++ parse/install-with-traefik.sh
@@ -5,10 +5,12 @@
 mkdir -p postgres-data
 
 read -p "Indicate your domain for parse dashboard : " domaine
-sed -i 's/URL_PARSE_DASH=/URL_PARSE_DASH='$domaine'/g' .env 
+sed -i '/URL_PARSE_DASH=/c\URL_PARSE_DASH='$domaine .env 
 
 read -p "Indicate your domain for parse server  : " domaine
-sed -i 's/URL_PARSE_SRV=/URL_PARSE_SRV='$domaine'/g' .env 
+sed -i '/URL_PARSE_SRV=/c\URL_PARSE_SRV='$domaine .env 
+
+sed -i 's/xxxx/'$domaine'/g' config.json
 
 docker-compose -f docker-compose-with-traefik.yml up -d
 
