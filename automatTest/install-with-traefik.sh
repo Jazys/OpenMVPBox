5ca4cb537a04162385f52b073935069940cd9dc8
Jazys <jacquemet.julien@gmail.com>
2021-09-04 09:07:24 +0000

    minor modification for writting a good env file

--- automatTest/install-with-traefik.sh
+++ automatTest/install-with-traefik.sh
@@ -8,10 +8,10 @@ cp configSftp.json configSftp/sftp.json
 mkdir -p TestReport
 
 read -p "Indicate your domain for e2e test: " domaine
-sed -i 's/URL_REPORT=/URL_REPORT='$domaine'/g' .env 
+sed -i '/URL_REPORT=/c\URL_REPORT='$domaine .env 
 
 read -p "Indicate your domain for sftp client : " domaine
-sed -i 's/URL_CLI_SFTP=/URL_CLI_SFTP='$domaine'/g' .env 
+sed -i '/URL_CLI_SFTP=/c\URL_CLI_SFTP='$domaine .env 
 
 docker-compose -f docker-compose-with-traefik.yml up -d
 
