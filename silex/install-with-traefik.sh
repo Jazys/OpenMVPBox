54d4eaf6c87b03050902175feb4764f275c5af8b
Jazys <jacquemet.julien@gmail.com>
2021-09-04 10:57:01 +0000

    add run docker compose in install

--- silex/install-with-traefik.sh
+++ silex/install-with-traefik.sh
@@ -5,4 +5,6 @@
 mkdir data
 mkdir data_website
 read -p "Indicate your domain for silex: " domaineSilex
-sed -i 's/URL_SILEX/URL_SILEX='$domaineSilex'/g' .env
+sed -i '/URL_SILEX/c\URL_SILEX='$domaineSilex .env
+
+docker-compose -f docker-compose-with-traefik.yml up -d
