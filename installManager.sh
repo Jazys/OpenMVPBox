33e5e727f3c209d518d6b30f0069b27f7f3fed7d
Jazys <jacquemet.julien@gmail.com>
2021-09-05 19:49:27 +0000

    set param in install script

--- installManager.sh
+++ installManager.sh
@@ -6,12 +6,26 @@ pushd traefik
 ./install.sh
 popd
 
-read -p "Indicate your domain for monitor traefik: " domaineTraefik
+domaineTraefik=""
+loginTraefik=""
+passTraefik=""
+domainePortainer=""
+
+if [ -z "$1" ]
+then
+      read -p "Indicate your domain for monitor traefik: " domaineTraefik
+      read -p "Indicate login for monitor traefik: " loginTraefik
+      read -p "Indicate password for monitor traefik: " passTraefik
+      read -p "Indicate your domain for portainer: " domainePortainer
+else
+      domaineTraefik=$1
+      loginTraefik=$2
+      passTraefik=$3
+      domainePortainer=$4
+      echo 'all domain  are ok'
+fi
+
 sed -i 's/xxx.xxx/'$domaineTraefik'/g' traefik/conf/traefik_dynamic.toml
-
-
-read -p "Indicate login for monitor traefik: " loginTraefik
-read -p "Indicate password for monitor traefik: " passTraefik
 
 htpasswd -b -c ./password $loginTraefik $passTraefik
 
@@ -24,7 +38,6 @@ sed -i '/admin/c\"'$EncryptedPassword'"' traefik/conf/traefik_dynamic.toml
 
 rm ./password
 
-read -p "Indicate your domain for portainer: " domainePortainer
 sed -i '/URL_PORTAINER/c\URL_PORTAINER = '$domainePortainer'' portainer/.env
 mkdir -p portainer/data
 
