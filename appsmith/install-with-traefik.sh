9ab4c8ef6a477f0997448e1b9e2234006584dbb3
Jazys <jacquemet.julien@gmail.com>
2021-09-05 19:57:39 +0000

    add parameter in install script

--- appsmith/install-with-traefik.sh
+++ appsmith/install-with-traefik.sh
@@ -2,7 +2,15 @@
 
 mkdir -p ./data/mongo/db
 
-read -p "Indicate your domain for Appsmith: " domaineAppsmith
+domaineAppsmith=""
+
+if [ -z "$1" ]
+then
+      read -p "Indicate your domain for Appsmith: " domaineAppsmith
+else
+      domaineAppsmith=$1
+      echo 'domaine for appsmith is 'domaineAppsmith
+fi
 sed -i '/URL_APPSMITH/c\URL_APPSMITH='$domaineAppsmith'' docker.env
 
 docker-compose -f docker-compose-with-traefik.yml --env-file docker.env up -d
