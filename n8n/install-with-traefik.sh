0a5e12488bb34299fa6af05646a92f1ff73ffbab
Jazys <jacquemet.julien@gmail.com>
2021-09-05 14:36:44 +0000

    fix error on traefik http and use  as paramter for NDD

--- n8n/install-with-traefik.sh
+++ n8n/install-with-traefik.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
 mkdir -p postgres-data
-#chown postgres:postgres postgres-data/ -R
-#find better solution !!!
-#chmod 777 postgres-data/ -R
-
 mkdir -p .n8n
 chmod +x init-data.sh
 
-read -p "Indicate your domain for n8n: " domaineN8n
+domaineN8n=""
+
+if [ -z "$1" ]
+then
+      read -p "Indicate your domain for n8n: " domaineN8n
+else
+      domaineN8n=$1
+      echo 'domaine for n8n is '$domaineN8n
+fi
 sed -i '/N8N_WEBHOOK_TUNNEL_URL/c\N8N_WEBHOOK_TUNNEL_URL=https://'$domaineN8n'/' .env
+sed -i '/URL_N8N/c\URL_N8N='$domaineN8n .env
 
 docker-compose -f docker-compose-with-traefik.yml --env-file .env up -d
