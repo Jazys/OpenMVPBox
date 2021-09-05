a641b9d300750756299ec996d6103d6757aeb77a
Jazys <jacquemet.julien@gmail.com>
2021-09-05 20:01:55 +0000

    add parameter for installNANStack

--- installNANStack.sh
+++ installNANStack.sh
@@ -3,14 +3,14 @@
 [ `whoami` = root ] || exec sudo su -c $0
 
 pushd n8n
-./install-with-traefik.sh
+./install-with-traefik.sh $1
 popd
 
 pushd appsmith
-./install-with-traefik.sh
+./install-with-traefik.sh $2
 popd
 
 pushd nocodb
-./install-with-traefik.sh
+./install-with-traefik.sh $3
 popd
 
