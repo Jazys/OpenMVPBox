ac3698a4bf254877a02c09839c9bce2ce25226d0
Jazys <jacquemet.julien@gmail.com>
2021-09-13 06:22:33 +0000

    generate password for portainer

--- portainer/install.sh
+++ portainer/install.sh
@@ -1,2 +1,14 @@
+
 #!/bin/bash
 mkdir data
+password=$(cat /dev/urandom | tr -dc '[:alpha:]' | fold -w ${1:-20} | head -n 1)
+echo "password généré" $password
+
+#hash=$(htpasswd -bn test $password | cut -c6-)
+#echo $hash
+#passordReplace=$(echo "${hash//$/\$\$}")
+#echo $passordReplace
+
+passwordCmdPortainerDoc=$(htpasswd -nb -B admin $password | cut -d ":" -f 2)
+echo $passwordCmdPortainerDoc
+sed -i '/PASSWORD_PORTAINER=/c\PASSWORD_PORTAINER='$passwordCmdPortainerDoc'' .env
