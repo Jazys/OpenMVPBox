2f9b5364c9b992ea0f31e3346185dd2a8bc6a050
Jazys <jacquemet.julien@gmail.com>
2021-09-14 05:46:43 +0000

    add a webhook n8n for sending mail

---
+++ sendMailWebHook.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+[ `whoami` = root ] || exec sudo su -c $0
+
+export PUBLIC_IPV4=$(curl -s http://169.254.169.254/metadata/v1/interfaces/public/0/ipv4/address)
+
+curl -X POST -H "Content-Type: text/plain" -d '{ "dest": "jacquemet.julien@gmail.com", "context":"ip", "IP":"'"$PUBLIC_IPV4"'", "accessKey":"yferfVvV2XJ^NUzczr4YYQ*2iyXSGY7J" }' https://automate.omvpb.ovh/webhook/ef724898-b6d9-4387-9878-81ea4c74092e
