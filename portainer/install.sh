75580ee66f88a37efbfa4450fc659c98fa17e381
Jazys <jacquemet.julien@gmail.com>
2021-09-18 14:59:35 +0000

    generate login/password automatically for portainer and save it to a file in order to send info by mail

--- portainer/install.sh
+++ portainer/install.sh
@@ -1,16 +1,28 @@
 
 #!/bin/bash
 mkdir data
-password=$(cat /dev/urandom | tr -dc '[:alpha:]' | fold -w ${1:-20} | head -n 1)
-echo "password généré" $password
 
-#hash=$(htpasswd -bn test $password | cut -c6-)
-#echo $hash
-#passordReplace=$(echo "${hash//$/\$\$}")
-#echo $passordReplace
+domainePortainer=""
+
+if [ -z "$1" ]
+then
+      read -p "Indicate your domain for portainer: " domainePortainer
+else
+      domainePortainer=$4
+      echo 'Domaine of portainer is ' $domainePortainer
+fi
+
+sed -i '/URL_PORTAINER/c\URL_PORTAINER = '$domainePortainer'' .env
+
+password=$(tr -dc A-Za-z0-9 </dev/urandom | head -c 13)
+echo "password généré" $password
 
 passwordCmdPortainerDoc=$(htpasswd -nb -B admin $password | cut -d ":" -f 2)
 echo $passwordCmdPortainerDoc
 sed -i '/PASSWORD_PORTAINER=/c\PASSWORD_PORTAINER='$passwordCmdPortainerDoc'' .env
 
-echo "Password pour le login admin de Portainer : " $password >> /tmp/mail
+echo "Login/admin for "$domainePortainer " are admin and "$password " " >> /tmp/toSendInfoByMail
+
+
+
+docker-compose up -d
