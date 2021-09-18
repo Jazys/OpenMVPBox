0f8e77a7cd2435f00d60b654a4dcb9ce84ba5610
Jazys <jacquemet.julien@gmail.com>
2021-09-18 14:57:33 +0000

    generate login/password automatically and save it to a file in order to send info by mail

--- traefik/install.sh
+++ traefik/install.sh
@@ -1,4 +1,35 @@
 #!/bin/bash
+
 docker network create traefik-proxy
 touch conf/acme.json
 chmod 600 conf/acme.json
+
+loginTraefik="admin"
+domaineTraefik=""
+
+password=$(tr -dc A-Za-z0-9 </dev/urandom | head -c 13)
+echo "password généré" $password
+
+if [ -z "$1" ]
+then
+      read -p "Indicate your domain for treafik: " domaineTraefik
+else
+      domaineTraefik=$1
+      echo 'domaine for traefik is '$domaineTraefik
+fi
+
+sed -i 's/xxx.xxx/'$domaineTraefik'/g' conf/traefik_dynamic.toml
+
+echo "Login/admin for "$domaineTraefik " are "$loginTraefik " and "$password " " >> /tmp/toSendInfoByMail
+
+htpasswd -b -c password $loginTraefik $password
+
+EncryptedPassword=$( cat ./password)
+echo $EncryptedPassword
+
+sed -i '/admin/c\"'$EncryptedPassword'"' conf/traefik_dynamic.toml
+
+rm ./password
+
+
+docker-compose up -d
