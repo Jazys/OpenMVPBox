45114510fc0665f013dd9e04d9b63c76d9af841b
Jazys <jacquemet.julien@gmail.com>
2021-09-18 15:53:03 +0000

    for sending creditential info (login/pass) by mail

---
+++ sendMailWebHookCreditentials.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+[ `whoami` = root ] || exec sudo su -c $0
+
+#work only in all data in a single line
+Info=$(cat /tmp/toSendInfoByMail | tr -d '\r\n')
+
+curl -X POST -H "Content-Type: text/plain" -d '{ "dest": "'"$1"'", "context":"info", "INFO":"'"$Info"'", "accessKey":"yferfVvV2XJ^NUzczr4YYQ*2iyXSGY7J" }' https://automate.omvpb.ovh/webhook/ef724898-b6d9-4387-9878-81ea4c74092e
+
