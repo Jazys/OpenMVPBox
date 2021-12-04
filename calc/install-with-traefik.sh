7a6920f9cd8238fe0c24017434aa4bf048f1ad8c
Jazys <jacquemet.julien@gmail.com>
2021-12-04 22:46:57 +0000

    add calc alias calendso, it's work randomly git status sometimes webpages can be reach sometimes not ... Need to work for improce stability

---
+++ calc/install-with-traefik.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+mkdir -p data-postgres
+git clone --recursive https://github.com/calendso/docker.git calendso
+cp calendso/* . -R
+
+domaine=""
+
+if [ -z "$1" ]
+then
+      read -p "Indicate your domain for calc: " domaine
+else
+      domaine=$1
+      echo 'domaine for calc is '$domaine
+fi
+
+sed -i '/URL_CALC/c\URL_CALC='$domaine .env
+sed -i '/URL_ADMIN_CONF/c\URL_ADMIN_CONF=conf-'$domaine .env
+
+password=$(tr -dc A-Za-z0-9 </dev/urandom | head -c 13)
+echo "password généré" $password
+cred=$(echo $(htpasswd -nb admin $password) | sed -e s/\\$/\\$\\$/g)
+echo "Login/admin for "$domaine " are admin and "$password " " >> /tmp/toSendInfoByMail
+
+echo $cred
+sed -i 's/$creditentials/'$cred'/g' docker-compose-with-traefik.yml
+
+sed -i '/NEXT_PUBLIC_APP_URL/c\NEXT_PUBLIC_APP_URL=https://'$domaine .env
+
+
+#docker-compose -f docker-compose-with-traefik.yml --env-file .env up 
