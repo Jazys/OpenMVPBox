d87dc6e91e81ec0102af5e0c4a2e39432015bc5e
Jazys <jacquemet.julien@gmail.com>
2021-12-05 15:07:04 +0000

    add grafana with traefik, need to write custom config gor user access

---
+++ grafana/install-with-traefik.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+mkdir -p data
+
+domaine=""
+
+if [ -z "$1" ]
+then
+      read -p "Indicate your domain for grafana: " domaine
+else
+      domaine=$1
+      echo 'domaine for grafana is '$domaine
+fi
+
+sed -i '/URL_GRAFANA/c\URL_GRAFANA='$domaine .env
+
+docker-compose -f docker-compose-with-traefik.yml --env-file .env up 
