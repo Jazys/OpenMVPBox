a801bedccc4131ed12518c1fb6926f72bd6d9d17
Jazys <jacquemet.julien@gmail.com>
2022-01-16 09:35:39 +0000

    add creditentials for frontend service

--- frontopenmvpbox/install.sh
+++ frontopenmvpbox/install.sh
@@ -9,5 +9,7 @@ echo "URL_FRONT=front."$1 > .env
 echo "VITE_URL_SRV=https://api."$1 >> .env
 echo "NETWORK_TRAEFIK=traefik-proxy" >> .env
 
+echo "creditentials are same as traefik service ">> /tmp/toSendInfoByMail
+
 docker-compose up -d
 
