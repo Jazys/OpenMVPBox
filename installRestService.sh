e818f1985104be2132eb8e2896eff13ca9926058
Jazys <jacquemet.julien@gmail.com>
2022-01-16 15:34:31 +0000

    fix error if no interface ip is found (eth1, enp3s0,...)

--- installRestService.sh
+++ installRestService.sh
@@ -13,6 +13,27 @@ echo "apkey for mananging stack via rest service "$apkeyhash
 echo "apkey for mananging stack via rest service "$apkeyhash >> /tmp/toSendInfoByMail
 echo $apkeyhash > /root/OpenMVPBox/apiKey
 
+localip=$(ip -4 addr show enp3s0 | grep -oP '(?<=inet\s)\d+(\.\d+){3}')
+
+testEth1=$(ip a |grep eth1 | wc -l)
+
+if (( testEth1 >= 1 )); then
+  localip=$(ip -4 addr show eth1 | grep -oP '(?<=inet\s)\d+(\.\d+){3}')
+fi
+
+testEnp3s0=$(ip a |grep enp3s0 | wc -l)
+
+if (( testEnp3s0 >= 1 )); then
+  localip=$(ip -4 addr show testEnp3s0 | grep -oP '(?<=inet\s)\d+(\.\d+){3}')
+fi
+
+domaineName=$(echo $1 | cut --complement -d'.' -f 1)
+
+sed -i 's/yyy.yyy/api.'$domaineName'/g' traefik/conf/traefik_dynamic.toml
+sed -i 's/@ip/'$localip'/g' traefik/conf/traefik_dynamic.toml
+
+sed -i 's/@ip/'$localip'/g' omvpb.py
+
 cp omvpb.py /home/ubuntu/stacks
 cp omvpb-back.service /etc/systemd/system
 
@@ -20,8 +41,3 @@ systemctl daemon-reload
 systemctl enable omvpb-back.service
 systemctl start omvpb-back.service
 
-localip=$(ip -4 addr show enp3s0 | grep -oP '(?<=inet\s)\d+(\.\d+){3}')
-domaineName=$(echo $1 | cut --complement -d'.' -f 1)
-
-sed -i 's/yyy.yyy/api.'$domaineName'/g' traefik/conf/traefik_dynamic.toml
-sed -i 's/@ip/'$localip'/g' traefik/conf/traefik_dynamic.toml
