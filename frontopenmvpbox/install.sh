10c7b55f096aee27e671c3f5b9b6e9bc2f737971
Jazys <jacquemet.julien@gmail.com>
2022-01-22 00:48:00 +0000

    add some information for configuration

--- frontopenmvpbox/install.sh
+++ frontopenmvpbox/install.sh
@@ -9,7 +9,9 @@ echo "URL_FRONT=front."$1 > .env
 echo "VITE_URL_SRV=https://api."$1 >> .env
 echo "NETWORK_TRAEFIK=traefik-proxy" >> .env
 
-echo "Frontend https://front.'$1' creditentials are same as traefik service ">> /tmp/toSendInfoByMail
+echo "Frontend https://front."$1" creditentials are same as traefik service ">> /tmp/toSendInfoByMail
+
+echo "For installing app, you need to use the frontend url, enter your login and password, go to configuration for adding the apkey, save and refresh your web page. Enjoy ">> /tmp/toSendInfoByMail
 
 docker-compose up -d
 
