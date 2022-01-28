11b1df11d9f80436f3f368d283567cfb08319aed
Jazys <jacquemet.julien@gmail.com>
2022-01-28 09:24:34 +0000

    fix name directory for cal

--- cal/install-with-traefik.sh
+++ cal/install-with-traefik.sh
@@ -18,11 +18,12 @@ sed -i '/URL_CALC/c\URL_CALC='$domaine .env
 sed -i '/URL_ADMIN_CONF/c\URL_ADMIN_CONF=conf-'$domaine .env
 
 password=$(tr -dc A-Za-z0-9 </dev/urandom | head -c 13)
-echo "password généré" $password
+#echo "password généré" $password
 cred=$(echo $(htpasswd -nb admin $password) | sed -e s/\\$/\\$\\$/g)
+echo "Login/admin for "$domaine " are admin and "$password " for conf-"$domaine
 echo "Login/admin for "$domaine " are admin and "$password " " >> /tmp/toSendInfoByMail
 
-echo $cred
+#echo $cred
 sed -i 's/$creditentials/'$cred'/g' docker-compose-with-traefik.yml
 
 sed -i '/NEXT_PUBLIC_APP_URL/c\NEXT_PUBLIC_APP_URL=https://'$domaine .env
