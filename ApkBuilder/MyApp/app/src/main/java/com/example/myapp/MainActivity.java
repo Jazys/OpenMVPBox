7d2feadcd3c058ed90e57661574027baad84ae59
Jazys <jacquemet.julien@gmail.com>
2022-02-26 23:05:13 +0000

    add docker file to create an apk from an URL (twa more complex)

---
+++ ApkBuilder/MyApp/app/src/main/java/com/example/myapp/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.myapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        WebView myWebView = (WebView) findViewById(R.id.webview);
+myWebView.loadUrl("https://titi.fr");
+    }
+}
