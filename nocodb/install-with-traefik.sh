4f276f0a4d9c6b4420373fa2bfcc510ca17e1812
Jazys <jacquemet.julien@gmail.com>
2022-06-17 23:56:30 +0000

    add nocodb for Saas version

--- nocodb/install-with-traefik.sh
+++ nocodb/install-with-traefik.sh
@@ -13,6 +13,13 @@ else
 fi
 sed -i '/URL_NOCODB/c\URL_NOCODB='$domaineNoco'' .env
 
+if [ -z "$2" ]
+then
+      echo "default user id 1"
+else
+      sed -i '/USER_ID/c\USER_ID='$2 .env
+fi
+
 echo 'For NocoDB use this url https://'$domaineNoco' create a superadmin user for the first connexion ' >> /tmp/toSendInfoByMail
 
 docker-compose -f docker-compose-with-traefik.yml  up -d
